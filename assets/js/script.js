3baa1f25c178dcef01a4addd2ee0432f1e63cd03
James Ma <xinlongtaiduo@hotmail.com>
2023-01-04 00:35:02 -0500

    basic function completed

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,20 +1,24 @@
-let DAY = 0;
-let GOLD = [2,3,2,3,5,3,10, 2,5,5,2,3,3,20, 2,3,2,3,2,3,30, 2,5,5,3,2,3,50];
-let COLLECTED = 5;
+const GOLD = [2,3,2,3,5,3,10, 2,5,5,2,3,3,20, 2,3,2,3,2,3,30, 2,5,5,3,2,3,50];
+let PREVIOUSLYCOLLECTED = 0;
 let TODAYCOLLECTED = false;
 
+let DAY = 0;
 rewardCardHandler();
+
+let cardEl = document.querySelector("#reward-card-content-wrapper");
+let buttonEl = document.querySelector("#reward-collect-button");
 
 function rewardCardHandler() {
     for (let i=0; i<28; i++) {
         DAY = i + 1;
         let tier = tierHandler(GOLD[i]);
-        if(DAY <= COLLECTED){
+        if(DAY <= PREVIOUSLYCOLLECTED){
             document.getElementById("left-calendar").appendChild(previouslyCollectedCard());
-        } else if (DAY == COLLECTED + 1) {
+        } else if (DAY == PREVIOUSLYCOLLECTED + 1) {
             if (TODAYCOLLECTED) {
-                document.getElementById("left-calendar").appendChild(previouslyCollectedCardToday());
+                document.getElementById("left-calendar").appendChild(previouslyCollectedCardToday(DAY));
                 document.getElementById("reward-card-content-wrapper").setAttribute("id", "reward-card-content-wrapper-today");
+                updateButton();
             } else {
                 document.getElementById("left-calendar").appendChild(defaultCardToday(DAY, GOLD[i], tier));
             }
@@ -22,20 +26,40 @@ function rewardCardHandler() {
             document.getElementById("left-calendar").appendChild(defaultCard(DAY, GOLD[i], tier));
         }
     }
+}
 
+// Card clicked to collect reward
+function cardCollectRewardHandler() {
+    TODAYCOLLECTED = true;
+    updateCardCollected();
+    updateButton();
+}
+
+// Button clicked to collect reward
+function buttonCollectRewardHandler() {
+    if(document.querySelector("#reward-card-today")){
+        TODAYCOLLECTED = true;
+        updateCardCollected();
+        updateButton();
+    }
 }
 
 // Determin the tier of the reward.
 // There are 6 tiers in the current reward. By dividing the total gold by 5, the result can be fall into different categories.
 function tierHandler(gold) {
-    switch(gold / 5) {
+    switch(parseInt(gold / 5)) {
         case 0: return 1;
         case 1: return 2;
         case 2: return 3;
+        case 3: return 3;
         case 4: return 4;
+        case 5: return 4;
         case 6: return 5;
+        case 7: return 5;
+        case 8: return 5;
+        case 9: return 5;
         case 10: return 6;
-        default: return 1;
+        default: return 6;
     }
 }
 
@@ -94,11 +118,11 @@ function defaultCardToday(day, gold, tier) {
     return card;
 }
 
-function previouslyCollectedCardToday() {
+function previouslyCollectedCardToday(today) {
     let card = document.createElement('div');
     card.setAttribute("id", "reward-card-collected-today");
     // import current day default card
-    card.innerHTML += defaultCardToday(DAY, GOLD[DAY-1], tierHandler(GOLD[DAY-1])).innerHTML;
+    card.innerHTML += defaultCardToday(today, GOLD[today-1], tierHandler(GOLD[today-1])).innerHTML;
     // add mask and collected tag
     card.innerHTML += `
         <div id="mask-today">
@@ -108,3 +132,26 @@ function previouslyCollectedCardToday() {
     return card;
 }
 
+function updateCardCollected() {
+    document.querySelector("#reward-card-today").replaceWith(previouslyCollectedCardToday(PREVIOUSLYCOLLECTED+1));
+    document.getElementById("reward-card-content-wrapper").setAttribute("id", "reward-card-content-wrapper-today");
+}
+
+function updateButton() {
+    document.querySelector("#reward-collect-button").replaceWith(rewardCollectedButton())
+}
+
+function rewardCollectedButton(){
+    let button = document.createElement('div');
+    button.setAttribute("id", "reward-collected-button");
+    button.innerHTML = `
+        <img src="./assets/imgs/svgs/check.svg" />
+        <span class="button-text">Reward Claimed Today</span>
+    `;
+    return button;
+}
+
+if(cardEl){
+    cardEl.addEventListener("click", cardCollectRewardHandler);
+}
+buttonEl.addEventListener("click", buttonCollectRewardHandler);
